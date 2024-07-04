db29f5c58fb0016c142e978b327d4fe9be3bef68
Aatif Ali <aatifwarraich10@gmail.com>
2024-07-04 16:03:06 +0500

    complete fitness tracker with all funcationality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,9 @@ import ProtectedRoute from './ProtectedRoute';
 import Login from './components/User_Account/Login';
 import Register from './components/User_Account/Register';
 import ProfilePage from './components/User_Account/ProfilePage';
-// import Profile from './components/User_Account/Profile';
-// import EditProfile from './components/User_Account/EditProfile';
-// import Website from './components/Website';
+import Profile from './components/User_Account/Profile';
+import EditProfile from './components/User_Account/EditProfile';
+import Website from './components/Website';
 
 const App = () => {
   return (
@@ -23,10 +23,10 @@ const App = () => {
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profilepage" element={<ProtectedRoute element={ProfilePage} />} />
-            {/* <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
+            <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
             <Route path="/update/:userId" element={<ProtectedRoute element={EditProfile} />} />
             <Route path="/db/*" element={<ProtectedRoute element={Db} />} />
-            <Route path="/web" element={<Website />} /> */}
+            <Route path="/web" element={<Website />} />
           </Routes>
         </div>
       </BrowserRouter>
